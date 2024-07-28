7b18fad57a363241bf816e6f28e120be9cd8ab79
mhe135 <mhe135@uclive.ac.nz>
2024-07-28 12:43:02 +1200

    Cleaning data script

--- 00_clean.R
+++ 00_clean.R
@@ -8,3 +8,19 @@ library(nycflights13)
 
 # Reading flights dataset
 head(flights)
+
+# Select necessary variables: carrier, dep_delay, arr_delay
+flights_clean <- flights %>%
+  select(carrier, dep_delay, arr_delay)
+
+# Summarize the mean departure and arrival delays by carrier
+flights_summary <- flights_clean %>%
+  group_by(carrier) %>%
+  summarize(
+    mean_dep_delay = mean(dep_delay, na.rm = TRUE),
+    mean_arr_delay = mean(arr_delay, na.rm = TRUE)
+  ) %>%
+  arrange(mean_dep_delay)
+
+# Display the summarized data
+print(flights_summary)
