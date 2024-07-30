9de9b889cf9a87222331a7594c5bcd91b63bd605
mhe135 <mhe135@uclive.ac.nz>
2024-07-30 17:41:34 +1200

    Installing and Loading Tidyverse and nycflights13

--- 00_clean.R
+++ 00_clean.R
@@ -1,66 +1,13 @@
-#3.3:
-# Check if tidyverse package is installed and install if not
+library(nycflights13)
+library(dplyr)
+library(data.table)
+
+# 3.3 Installing and loading Tidyverse and nycflights13 packages
 if (!requireNamespace("tidyverse", quietly = TRUE)) {
   install.packages("tidyverse")
 }
-
-# Check if nycflights13 package is installed and install if not
 if (!requireNamespace("nycflights13", quietly = TRUE)) {
   install.packages("nycflights13")
 }
-
-# Load the packages
 suppressPackageStartupMessages(library(tidyverse))
-suppressPackageStartupMessages(library(nycflights13))
-
-#3.4
-# Reading flights dataset
-head(flights)
-
-#3.5:
-# Load the Tidyverse library, which includes dplyr
-library(tidyverse)
-# Selecting only the carrier and delay variables
-flights_clean <- flights %>%
-  select(carrier, dep_delay, arr_delay)
-# Summarizing the mean departure and arrival delays by carrier
-flights_summary <- flights_clean %>%
-  group_by(carrier) %>%
-  summarize(
-    mean_dep_delay = mean(dep_delay, na.rm = TRUE),
-    mean_arr_delay = mean(arr_delay, na.rm = TRUE),
-    .groups = 'drop'  # Drop grouping for easier further manipulation
-  ) %>%
-  arrange(mean_dep_delay)
-# Printing the summary to check the results
-print(flights_summary)
-
-#3.6:
-# Load necessary libraries
 library(nycflights13)
-library(dplyr)
-# Load the datasets
-data("flights")
-data("airlines")
-# Create a summary of mean delays by carrier
-flights_summary <- flights %>%
-  select(carrier, dep_delay, arr_delay) %>%
-  group_by(carrier) %>%
-  summarize(
-    mean_dep_delay = mean(dep_delay, na.rm = TRUE),
-    mean_arr_delay = mean(arr_delay, na.rm = TRUE),
-    .groups = 'drop')
-# Join the airlines names onto the flights summary
-flights_with_airlines <- flights_summary %>%
-  left_join(airlines, by = "carrier") %>%
-  select(-carrier, name, mean_dep_delay, mean_arr_delay) %>%
-  rename(airline_name = name)
-# Join the airlines names onto the flights summary
-flights_with_airlines <- flights_summary %>%
-  left_join(airlines, by = "carrier") %>%
-  select(-carrier, name, mean_dep_delay, mean_arr_delay) %>%
-  rename(airline_name = name)
-head(flights_with_airlines)
-
-
-
